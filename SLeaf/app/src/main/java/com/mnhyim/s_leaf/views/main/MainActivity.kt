614fe367eb48d89ffb81523531db5287aecf910a
Muhamad Nur Hasyim <mnhyim76@gmail.com>
2021-05-30 00:52:44 +0800

    package structure

---
+++ SLeaf/app/src/main/java/com/mnhyim/s_leaf/views/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.mnhyim.s_leaf.views.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mnhyim.s_leaf.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
