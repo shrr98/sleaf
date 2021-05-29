fc9f3cd3dfc86259066d2608f9601517cc8607ba
Muhamad Nur Hasyim <mnhyim76@gmail.com>
2021-05-30 00:57:31 +0800

    add splashscreen and resources

---
+++ SLeaf/app/src/main/java/com/mnhyim/s_leaf/views/splash/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.mnhyim.s_leaf.views.splash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mnhyim.s_leaf.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
