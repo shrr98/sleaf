fce27e45ee09a29b68fe070298edbf04a5fe40da
Muhamad Nur Hasyim <mnhyim76@gmail.com>
2021-05-30 08:12:22 +0800

    favorite fragments

--- SLeaf/app/src/main/java/com/mnhyim/s_leaf/views/favorite/FavoriteFragment.kt
+++ SLeaf/app/src/main/java/com/mnhyim/s_leaf/views/favorite/FavoriteFragment.kt
@@ -6,7 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.mnhyim.s_leaf.R
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.mnhyim.s_leaf.core.domain.model.Plant
 import com.mnhyim.s_leaf.core.ui.FavoriteAdapter
 import com.mnhyim.s_leaf.databinding.FragmentFavoriteBinding
@@ -30,31 +30,40 @@ class FavoriteFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        // TODO: DEBUG
-
-        favoriteBinding.btnDebug.setOnClickListener {
-            val c = Calendar.getInstance()
-            val x = c.get(Calendar.SECOND)
-            favoriteViewModel.addFavorite(
-                Plant(
-                    id = 0,
-                    className = "Classname $x",
-                    name = "Name $x",
-                    desc = "Desc  $x",
-                    scientificName = "ScientificName $x",
-                    imageURL = "imageURL $x",
-                    isFavorite = true
-                )
-            )
-        }
-
         if (activity != null) {
 
+            // TODO: DEBUG BUTTON, hapus nnti
+            favoriteBinding.btnDebug.setOnClickListener {
+                val c = Calendar.getInstance()
+                val x = c.get(Calendar.SECOND)
+                favoriteViewModel.addFavorite(
+                    Plant(
+                        id = 0,
+                        className = "Classname $x",
+                        name = "Name $x",
+                        desc = "Desc  $x",
+                        scientificName = "ScientificName $x",
+                        imageURL = "imageURL $x",
+                        isFavorite = true
+                    )
+                )
+            }
+
+            val favoriteAdapter = FavoriteAdapter()
+            favoriteAdapter.onItemClick = { selectedData ->
+                Log.d("FavoriteFragment", "$selectedData clicked")
+            }
+
             favoriteViewModel.listFavorite.observe(viewLifecycleOwner, { dataFavorite ->
+                favoriteAdapter.setData(dataFavorite)
                 Log.d("FavoriteFragment", "${favoriteViewModel.listFavorite.value}")
             })
 
+            with(favoriteBinding.rvPlants) {
+                layoutManager = LinearLayoutManager(context)
+                setHasFixedSize(true)
+                adapter = favoriteAdapter
+            }
         }
     }
 }
